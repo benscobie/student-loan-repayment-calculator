db0f4df0d2064566e162c8283665e6ff3a05405b
Ben Scobie <benscobie@users.noreply.github.com>
2023-05-01 08:08:01 +0100

    Add a share feedback link

--- components/ui/organisms/header.tsx
+++ components/ui/organisms/header.tsx
@@ -1,8 +1,13 @@
 const Header = () => {
   return (
-    <header className="bg-sky-600 text-white font-semibold text-lg flex justify-center shadow">
-      <div className="max-w-7xl w-full py-4 px-8">
-        Student Loan Repayment Calculator
+    <header className="bg-sky-600 flex justify-center shadow">
+      <div className="flex w-full max-w-7xl py-4 px-8 text-white">
+        <div className="flex-grow font-semibold text-lg">
+          Student Loan Repayment Calculator
+        </div>
+        <div>
+          <a href="https://forms.gle/FWqyT5SUgumSKJG46">Share feedback</a>
+        </div>
       </div>
     </header>
   );
