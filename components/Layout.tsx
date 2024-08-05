3e10944801ffb45eb878e32cb8177500b7eb46dc
Ben Scobie <benscobie@users.noreply.github.com>
2024-08-05 22:29:50 +0100

    Add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Footer from "./ui/organisms/Footer";
 import Header from "./ui/organisms/Header";
+import Head from "next/head";
 
 interface LayoutProps {
   children?: ReactNode | undefined;
@@ -9,11 +10,33 @@ interface LayoutProps {
 
 function Layout({ children, className }: LayoutProps) {
   return (
-    <div className={className}>
-      <Header />
-      <main className="container mx-auto max-w-7xl my-8">{children}</main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <div className={className}>
+        <Header />
+        <main className="container mx-auto max-w-7xl my-8">{children}</main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
